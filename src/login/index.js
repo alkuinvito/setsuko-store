50cb0f5397c50fe8fe5bb1d220922559b696965e
Alkuin Vito <alkuinvito@tutanota.com>
2022-11-10 17:58:29 +0700

    Adding form selector

--- src/login/index.js
+++ src/login/index.js
@@ -1,46 +1,23 @@
-var submit = document.getElementById("submit");
-var r_submit = document.getElementById("r_submit");
-var username = document.getElementById("username");
-var password = document.getElementById("password");
-var r_username = document.getElementById("r_username");
-var r_password = document.getElementById("r_password");
-var blocker = document.getElementById("blocker");
-var pop_message = document.getElementById("pop_message");
+const form = document.getElementById("fm-auth");
+const label = document.getElementById("fm-label");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const submitButton = document.getElementById("btnSubmit");
+const submitLabel = document.getElementById("labelSubmit");
 
+let isSignIn = true;
+let handler = "login.php";
 
-function httpGetAsync(theUrl, callback, str1, str2) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', theUrl, true);
-    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xhr.onload = function () {
-        callback(this.responseText);
-    };
-    xhr.send('username=' + str1 + '&password=' + str2);
-};
-
-function popup(message) {
-    blocker.classList.remove("hide");
-    pop_message.innerHTML=message;
-    blocker.addEventListener("click", function() {
-        blocker.classList.add("hide");
-    });
-}
-
-submit.addEventListener("click", function() {
-    httpGetAsync("./login.php", function(evt) {
-        if(evt=="FALSE") {
-            console.log("Username and/or password incorrect.");
-        } else if(evt=="TRUE") {
-            console.log("200");
-            window.location.replace("./member/");
-        }
-    }, username.value, password.value);
-});
-r_submit.addEventListener("click", function() {
-    httpGetAsync("./register.php", console.log, r_username.value, r_password.value);
-});
-
-function toggleBox() {
-    document.getElementById("loginBox").classList.toggle("hide");
-    document.getElementById("registerBox").classList.toggle("hide");
+function toggleForm() {
+    if(isSignIn) {
+        isSignIn = false;
+        label.innerHTML = "Sign up";
+        labelSubmit.innerHTML = "Signup";
+        handler = "register.php";
+    } else {
+        isSignIn = true;
+        label.innerHTML = "Log In";
+        labelSubmit.innerHTML = "Login";
+        handler = "login.php";
+    }
 }
