40a1fa489d90ba2b33669786a6f9cfa45ebb8c8d
Alkuin Vito <alkuinvito@tutanota.com>
2022-11-10 19:58:13 +0700

    Building auth form

--- src/login/index.js
+++ src/login/index.js
@@ -1,23 +1,76 @@
+const host = "http://setsuko.store/";
+
 const form = document.getElementById("fm-auth");
-const label = document.getElementById("fm-label");
 const username = document.getElementById("username");
 const password = document.getElementById("password");
 const submitButton = document.getElementById("btnSubmit");
 const submitLabel = document.getElementById("labelSubmit");
+const accent = document.getElementById("fm-selector-accent");
+const labels = document.getElementsByClassName("fm-label");
+const message = document.getElementById("message");
+const loader = document.querySelector(".fa-spinner");
 
 let isSignIn = true;
 let handler = "login.php";
 
 function toggleForm() {
+    message.classList.add("hide");
     if(isSignIn) {
-        isSignIn = false;
-        label.innerHTML = "Sign up";
-        labelSubmit.innerHTML = "Signup";
         handler = "register.php";
+        for(elem of labels) {
+            elem.classList.toggle("active");
+        }
+        accent.style.transform = "translate(100%, -100%)";
+        submitLabel.innerHTML = "Signup";
+        isSignIn = false;
     } else {
-        isSignIn = true;
-        label.innerHTML = "Log In";
-        labelSubmit.innerHTML = "Login";
         handler = "login.php";
+        for(elem of labels) {
+            elem.classList.toggle("active");
+        }
+        accent.style.transform = "translate(0, -100%)";
+        submitLabel.innerHTML = "Signup";
+        isSignIn = true;
     }
 }
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    submitLabel.classList.add("hide");
+    loader.classList.remove("hide");
+    message.classList.add("hide");
+    if(username.value != "" && password.value != "") {
+        fetch(host + "/api/" + handler, {method: "POST", headers: {}, body:
+            new URLSearchParams({
+                username: username.value,
+                password: password.value
+            })
+        })
+        .then(response => response.json())
+        .then(response => {
+            if(response.success === true) {
+                if(isSignIn) {
+                    localStorage.setItem("token", response.token);
+                    window.location.href = "../";
+                } else {
+                    toggleForm();
+                }
+            } else {
+                if(isSignIn) {
+                    message.textContent = "Incorrect username or password";
+                    message.classList.remove("hide");
+                } else {
+                    message.textContent = "Username is already exist";
+                    message.classList.remove("hide");
+                }
+            }
+
+        })
+    } else {
+        message.textContent = "Username and password can not be empty";
+        message.classList.remove("hide");
+    }
+
+    submitLabel.classList.remove("hide");
+    loader.classList.add("hide");
+})
