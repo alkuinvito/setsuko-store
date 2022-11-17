9e1ba71a5fbe5071a514966fc4efb8b45850369e
Alkuin Vito <alkuinvito@tutanota.com>
2022-11-18 02:13:10 +0700

    Adding plus and minus product in cart

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -1,3 +1,46 @@
+function qtyAdd(id) {
+    let pos = -1;
+    let products = localStorage.getItem("products");
+    products = JSON.parse(products);
+
+    for(let i=0; i<products.length; i++) {
+        if(products[i].id == id) {
+            pos = i;
+            break;
+        }
+    }
+    
+    if(pos != -1) {
+        products[pos].qty += 1;
+        document.querySelector("li[data-id='"+id+ "'] .ca-qty").textContent = products[pos].qty;
+        localStorage.setItem("products", JSON.stringify(products));
+    }
+}
+
+function qtyDel(id) {
+    let pos = -1;
+    let products = localStorage.getItem("products");
+    products = JSON.parse(products);
+    
+    for(let i=0; i<products.length; i++) {
+        if(products[i].id == id) {
+            pos = i;
+            break;
+        }
+    }
+
+    if(pos != -1) {
+        if(products[pos].qty > 1) {
+            products[pos].qty -= 1;
+            document.querySelector("li[data-id='"+id+ "'] .ca-qty").textContent = products[pos].qty;
+        } else {
+            products.splice(pos, 1);
+            document.querySelector("li[data-id='"+id+ "']").remove();
+        }
+    }
+    localStorage.setItem("products", JSON.stringify(products));
+}
+
 function renderCart(host, list, products) {
     for(data of products) {
         const pdItem = document.createElement("li");
@@ -6,6 +49,9 @@ function renderCart(host, list, products) {
         const pdName = document.createElement("h3");
         const pdPrice = document.createElement("span");
         const pdQty = document.createElement("span");
+        const qtyPlus = document.createElement("button");
+        const qtyMinus = document.createElement("button");
+        const qtyWrapper = document.createElement("div");
 
         pdImg.src = host + data.image;
         pdName.textContent = data.name;
@@ -15,12 +61,26 @@ function renderCart(host, list, products) {
         pdItem.setAttribute("data-id", data.id);
         pdItem.className = "ca-item";
         pdInfo.className = "ca-info";
+        qtyPlus.className = "qty-plus";
+        qtyPlus.innerHTML = "<i class='fa-solid fa-plus'></i>";
+        qtyWrapper.setAttribute("data-id", data.id);
+        qtyPlus.addEventListener("click", function() {
+            qtyAdd(this.parentElement.dataset.id);
+        })
+        qtyMinus.className = "qty-minus";
+        qtyMinus.innerHTML = "<i class='fa-solid fa-minus'></i>"; 
+        qtyMinus.addEventListener("click", function() {
+            qtyDel(this.parentElement.dataset.id);
+        })
 
         pdItem.append(pdImg);
         pdInfo.append(pdName);
         pdInfo.append(pdPrice);
+        qtyWrapper.append(qtyPlus);
+        qtyWrapper.append(pdQty);
+        qtyWrapper.append(qtyMinus);
+        pdInfo.append(qtyWrapper);
         pdItem.append(pdInfo);
-        pdItem.append(pdQty);
         list.appendChild(pdItem);
     }
 }
